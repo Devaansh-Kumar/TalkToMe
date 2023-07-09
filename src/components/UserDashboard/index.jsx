58c1249f168078cc9425c786e5360db808d6e8c4
harshit <harshitgupta1406@gmail.com>
2023-07-09 16:37:43 +0530

    added user profile pic to navbar

--- src/components/UserDashboard/index.jsx
+++ src/components/UserDashboard/index.jsx
@@ -15,8 +15,6 @@ const UserDashboard = () => {
 {uploadedFiles.length? (<ul class="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
     {uploadedFiles.map(file=>(<UploadListItem file={file}/>))}
 </ul>) : <p>Loading...</p> }
-
-
     </>
   )
 }
